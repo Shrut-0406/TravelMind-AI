e1999fb3d29941738b26a75ec26d329c91397cf4
Shrut-0406 <Shrutshekhat@gmail.com>
2026-07-17 13:20:17 -0600

    Connected trip planner form to Flask backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,23 @@ def home():
 @app.route("/planner")
 def planner():
     return render_template("planner.html")
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+
+    start = request.form["start"]
+    destination = request.form["destination"]
+    days = request.form["days"]
+    budget = request.form["budget"]
+
+    return render_template(
+        "trip_result.html",
+        start=start,
+        destination=destination,
+        days=days,
+        budget=budget
+    )
 
 
 if __name__ == "__main__":
